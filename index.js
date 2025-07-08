c48f12c53cc4f59bdb4653bd1af74fb4fae1ce2f
Pathak Gaurav <pathakgauravepic@gmail.com>
2025-07-08 15:52:21 +0530

    code push  by gaurav

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const connectDB = require('./config/db');
 const authRoutes = require('./routes/auth.routes');
 const profileRoutes = require('./routes/profile.routes');
 const changePinRoutes = require('./routes/changePin.routes');
+const verifyPinRoute = require('./routes/verify-pin');
 const cors = require('cors');
 
 const app = express();
@@ -20,6 +21,7 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/profile', profileRoutes);
 app.use('/api/change-pin', changePinRoutes);
+app.use('/api/verify-pin', verifyPinRoute);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
