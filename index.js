a6788d390844e9b47e97f288d8615422748dce9a
Gaurav Pathak <pathakgauravepic@gmail.com>
2025-09-19 11:31:55 +0530

    Code Update By Pathak

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ const doctorRoutes = require('./routes/doctorRoutes');
 
 const cors = require('cors');
 
-const app = express();
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 
 connectDB();
