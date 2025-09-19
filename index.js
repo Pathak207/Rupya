b33e6b41934b95ed8ec1b455f3dad8cb336e541e
Gaurav Pathak <pathakgauravepic@gmail.com>
2025-09-19 11:37:04 +0530

    code update by Pathak

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config();
-const express = require('express');
+
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/auth.routes');
 const profileRoutes = require('./routes/profile.routes');
@@ -9,18 +10,18 @@ const verifyPinRoute = require('./routes/verify-pin');
 const forgetPinRoutes = require('./routes/forget-pin');
 const walletRoutes = require('./routes/walletRoutes');
 const doctorRoutes = require('./routes/doctorRoutes');
-
 const cors = require('cors');
 
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
+const app = express();
 
 
 connectDB();
 
-
 app.use(cors());
-app.use(express.json());
+
+
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 
 app.use('/api/auth', authRoutes);
@@ -30,7 +31,6 @@ app.use('/api/verify-pin', verifyPinRoute);
 app.use('/api/forget-pin', forgetPinRoutes);
 app.use('/api/wallet', walletRoutes);
 app.use('/api/doctor', doctorRoutes);
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
