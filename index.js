74875af1c28532126790c5e9c70e387658d136c8
Gaurav Pathak <pathakgauravepic@gmail.com>
2025-09-19 12:31:20 +0530

    Code update by Gaurav

--- index.js
+++ index.js
@@ -22,7 +22,8 @@ app.use(cors());
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
-
+//for folder to public
+app.use('/uploads', express.static('uploads'));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/profile', profileRoutes);
